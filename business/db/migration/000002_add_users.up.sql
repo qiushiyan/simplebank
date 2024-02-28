86c16d2e3944fe9f6b3d9ed8f74ebc5c579b5ad8
Qiushi Yan <qiushi.yann@gmail.com>
2024-02-28 16:19:28 -0600

    Update 000002_add_users.up.sql

--- business/db/migration/000002_add_users.up.sql
+++ business/db/migration/000002_add_users.up.sql
@@ -10,3 +10,10 @@ ADD FOREIGN KEY ("owner") REFERENCES "users" ("username");
 -- CREATE UNIQUE INDEX ON "accounts" ("owner", "currency");
 ALTER TABLE "accounts"
 ADD CONSTRAINT "owner_currency_key" UNIQUE ("owner", "currency");
+-- create admin user
+INSERT INTO "users" ("username", "email", "hashed_password")
+VALUES (
+        'admin',
+        'test@gmail.com',
+        '$2a$10$SZw5eHfb.e94cBVUbrZ0BOTmXOFtINl7ZOv9Ac9WbYJdOnNZ9Gk6a'
+    );
